3c52a2e0289cf075a456425ed8fbd5181fe032b2
User <ntuma2022@gmail.com>
2025-05-27 17:21:19 +0400

    four steps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TestPageComponent } from './test-page/test-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'test-page',
+        component: TestPageComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
